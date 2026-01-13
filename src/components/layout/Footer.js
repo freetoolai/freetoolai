b4e7f56d5e33544fa5047170d2cb8ba4e20549c8
penguinpayindia <penguinpay.store@gmail.com>
2026-01-13 20:03:08 +0530

    Fix: Add use client to Footer for event handlers

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from 'next/link';
 import { Twitter, Github, Globe, Mail, Linkedin, Instagram, Facebook, Youtube, Play, Shield, User, LogOut } from 'lucide-react';
 import styles from './Footer.module.css';
